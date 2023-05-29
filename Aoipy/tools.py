61f94a9ad0e28fb951e782858770a3c2c3bb83e4
Jade <jaylan.franklin09@gmail.com>
2023-05-28 23:17:40 -0500

    Made function a bit nicer

--- Aoipy/tools.py
+++ Aoipy/tools.py
@@ -4,11 +4,8 @@ funcs = {"$username": username, "$authorid": authorID, "$send": send, "$pyeval":
 
 
 async def findBracketPairs(entry: str):
-    all = entry.split("\n")
-    for code in all:
-        if len(code) < 3:
-            continue
-        code = code.strip()
+    lines = [line.strip() for line in entry.split("\n") if len(line.strip()) >= 3]
+    for code in lines:
         first = None
         last = None
         argument = ""
@@ -17,17 +14,13 @@ async def findBracketPairs(entry: str):
         for i in code:
             if i == "[" and first is None:
                 first = count
-                count += 1
-                continue
-            if i == "]":
+            elif i == "]":
                 last = count
-                count += 1
-                continue
             count += 1
         argument = code[first + 1:last]
         keyword = code[0:first]
         find = [first, last, keyword, argument]
-        while "[" in str(argument) and "]" in str(argument) and "$" in str(argument):
+        while "[" in argument and "]" in argument and "$" in argument:
             count = 0
             start = None
             end = None
@@ -35,26 +28,25 @@ async def findBracketPairs(entry: str):
             for i in argument:
                 if i == "$" and start is None:
                     start = count
-                    count += 1
-                    continue
-                if i == "[":
+                elif i == "[":
                     balance += 1
-                if i == "]":
+                elif i == "]":
                     end = count
-                    count += 1
                     balance -= 1
-                    continue
                 count += 1
                 if balance == 0 and start is not None and end is not None:
                     break
+
             if start != 0:
-                argument = str(argument[0:start]) + str(await findBracketPairs(argument[start:end + 1])) + argument[end + 1: len(argument)]
+                argument = argument[:start] + await findBracketPairs(argument[start:end + 1]) + argument[end + 1:]
             else:
                 argument = await findBracketPairs(argument)
             find = [first, last, keyword, argument]
         if find[2].lower() in funcs:
             name = await funcs[find[2].lower()](find[3])
+
     try:
         return name
     except:
         raise SyntaxError(f"Missing '$' in {code}")
+
