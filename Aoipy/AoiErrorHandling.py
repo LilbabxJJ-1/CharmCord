67cbd0b5d9b9307684af9c1752a5a62e13a22b80
Jade <jaylan.franklin09@gmail.com>
2023-05-30 01:32:17 -0500

    More Error codes

--- Aoipy/AoiErrorHandling.py
+++ Aoipy/AoiErrorHandling.py
@@ -2,4 +2,9 @@ class AoipyErrorHandling:
 
     def Errors(self, error, code):
         if error == 1:
-            raise SyntaxError(f"ID {code} is not a user ID")
+            raise SyntaxError(f"ID '{code}' is not a user ID")
+        if error == 2:
+            raise SyntaxError(f"ID '{code}' is not a channel ID")
+
+        if error == 4:
+            raise SyntaxError(code)
