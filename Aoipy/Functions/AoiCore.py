46fed3903fb7064e1e0b8ca96efb867035286684
Jade <jaylan.franklin09@gmail.com>
2023-05-29 19:39:34 -0500

    Comments

--- Aoipy/Functions/AoiCore.py
+++ Aoipy/Functions/AoiCore.py
@@ -3,16 +3,22 @@ from discord.ext import commands
 from Aoipy.tools import findBracketPairs, FunctionHandler
 from Aoipy.Functions import *
 
+# Global variables
 global bots
 global Context
 global TotalFuncs
 
+
 class Start(discord.Client):
+    # Global variables
     global bots
     global Context
 
     def __init__(self, prefix: str, case_insensitive: bool = False, intents: tuple = ("default",), activity=None, help_command=None):
+        # Global variables
         global bots
+
+        # Initialize Start class
         self.prefix = prefix
         self.case_insensitive = case_insensitive
         self.intented = intents
@@ -20,12 +26,16 @@ class Start(discord.Client):
         self._clients = ''
         self.intent = ''
         self._activity = activity
+
+        # Determine intents
         if "all" in self.intented:
             self.intent = discord.Intents.all()
         elif "default" in self.intented:
             self.intent = discord.Intents.default()
         else:
             self.intent = discord.Intents.default()
+
+        # Enable specific intents
         if "message" in self.intented:
             self.intent.message_content = True
         if "members" in self.intented:
@@ -33,6 +43,7 @@ class Start(discord.Client):
         if "presences" in self.intented:
             self.intent.presences = True
 
+        # Create bot instances
         if self._activity is None:
             self._clients = commands.Bot(command_prefix=self.prefix, case_insensitive=self.case_insensitive, intents=self.intent,
                                          help_command=self._help_command)
@@ -55,16 +66,20 @@ class Start(discord.Client):
 
 
 class Commands:
+    # Global variables
     global bots
     global Context
 
     def command(self, Name, Code):
+        # Define command function dynamically
         @bots.command(name=Name)
         async def go(ctx, *args, Code=Code):
             global Context
             Context = ctx
             getChannelContext(Context)
             getGuildContext(Context)
+
+            # Handle $args substitution
             if '$args' in Code:
                 while "$args" in str(Code):
                     count = 0
@@ -84,6 +99,7 @@ class Commands:
                         count += 1
                         if balance == 0 and start is not None and end is not None:
                             try:
+                                # Replace $args with arguments
                                 Code = str(Code).replace(f"$args[{look[start + 1:end]}]", args[int(look[start + 1:end]) - 1])
                                 break
                             except IndexError:
@@ -97,17 +113,23 @@ class Commands:
 
 class AoiEvents:
     def onReady(self, code):
+        # Define on_ready event function
         @bots.event
         async def on_ready():
             await findBracketPairs(code, TotalFuncs)
 
 
 def Bot(prefix: str, case_insensitive: bool = False, intents: tuple = ("default",), activity=None, help_command=None):
+    # Global variables
     global bots
     global TotalFuncs
+
+    # Initialize FunctionHandler and register functions
     Functions = FunctionHandler()
     TotalFuncs = Functions
     Functions.register_functions()
+
+    # Create Start instance and return working bot
     _final = Start(prefix, case_insensitive, intents, activity, help_command)
     working = _final.clients
     return working
