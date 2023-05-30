90d130ab35ec9a6155217adb0dd22aafe0fb7280
Jade <jaylan.franklin09@gmail.com>
2023-05-30 01:33:43 -0500

    Added channel Functions

--- Aoipy/Functions/Guilds/Channels.py
+++ Aoipy/Functions/Guilds/Channels.py
@@ -1,6 +1,8 @@
 import discord
+from Aoipy.AoiErrorHandling import AoipyErrorHandling
 
 global Context
+Errors = AoipyErrorHandling()
 
 
 def getChannelContext(ctx):
@@ -16,11 +18,32 @@ async def currentChannelName(empty):
     return Context.channel.name
 
 
-async def getChannelDelay(ID):
+async def channelName(ID):
     from Aoipy.Functions.AoiCore import bots
     try:
         int(ID)
         channel = await bots.fetch_user(ID)
+        return channel.name
+    except ValueError:
+        Errors.Errors(2, ID)
+
+
+async def channelID(Name):
+    from Aoipy.Functions.AoiCore import bots
+    try:
+        channel = await bots.fetch_user(Name)
+        return channel.name
+    except ValueError:
+        Errors.Errors(2, Name)
+
+
+async def channelDelay(ID):
+    if len(ID) < 1:
+        raise Errors.Errors(4, "No parameter provided for '$channelDelay'")
+    from Aoipy.Functions.AoiCore import bots
+    try:
+        int(ID)
+        channel = await bots.fetch_channel(ID)
         return channel.slowmode_delay
     except ValueError:
-        raise Exception(f"{ID} isn't a Channel ID")
+        Errors.Errors(2, ID)
