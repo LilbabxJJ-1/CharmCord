d2e207aff29ef9f365c45177c0abb9f9eeb7110c
Jade <jaylan.franklin09@gmail.com>
2023-06-02 14:05:54 -0500

    Adding Asyncio to bot.add_cog

--- Aoipy/Classes/Cogs.py
+++ Aoipy/Classes/Cogs.py
@@ -1,8 +1,9 @@
 from discord.ext import commands
 from Aoipy.tools import findBracketPairs, checkArgs, checkArgCheck
+import asyncio
+
 
 class cogs:
-    
     def Cogs(self, Cog_Group, Name, Code):
         from Aoipy.Classes.AoiPyClient import bots
 
@@ -13,14 +14,11 @@ class cogs:
             @commands.command(name=Name)
             async def go(self, ctx, *args, Code=Code):
                 from Aoipy.Classes.AoiPyClient import TotalFuncs
-                Context   = ctx
-                newCode   = checkArgs(args, Code)
+                Context = ctx
+                newCode = checkArgs(args, Code)
                 finalCode = await checkArgCheck(args, newCode, Context)
 
                 await findBracketPairs(finalCode, TotalFuncs, Context)
 
-        
-            
-
         COGS = type(Cog_Group, (commands.Cog,), {'__init__': main_bod.__init__, 'go': main_bod.go})
-        bots.add_cog(COGS(bots))
+        asyncio.run(bots.add_cog(COGS(bots)))
