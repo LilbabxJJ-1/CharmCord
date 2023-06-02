0842bf9e0929ead872476a776abc73b0659636e6
Jade <jaylan.franklin09@gmail.com>
2023-06-02 14:07:08 -0500

    New command handle format

--- commands/test.py
+++ commands/test.py
@@ -1,5 +1,13 @@
-import discord
+from Aoipy import cogs
+command = cogs().Cogs
+command(
+    Name="Test",
+    Code="$sendMessage[$currentChannelID[];This works]",
+    Cog_Group="Testing"
+)
 
-
-def lol(text: discord.TextChannel):
-    text.mention
+command(
+    Name="Test2",
+    Code='$sendMessage[$currentChannelID[]; This works again]',
+    Cog_Group="Testing"
+)
