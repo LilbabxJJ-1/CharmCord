1d0ed94dda315b58992caedeb1b0976bcbf68132
Jade <jaylan.franklin09@gmail.com>
2023-06-03 15:57:05 -0500

    Fixed and renamed guildChannels.py

---
+++ Aoipy/Functions/Guilds/guildTextChannels.py
@@ -0,0 +1,20 @@
+import Aoipy.AoiErrorHandling as ErrorHandling
+
+EH = ErrorHandling.AoipyErrorHandling()
+import discord
+d = discord.Guild
+
+async def guildTextChannels(ID, Context):
+    from Aoipy.Classes.AoiPyClient import bots
+    try:
+        ID = int(ID)
+        text = []
+        guild = await bots.fetch_guild(ID)
+        channels = list(await guild.fetch_channels())
+        for channel in channels:
+            if isinstance(channel, discord.TextChannel):
+                text.append(channel.name)
+        return text
+    except ValueError:
+        EH.Errors(2, "None")
+
