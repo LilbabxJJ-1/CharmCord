8f345c7d9d9532529f584e2bd8a3b96e47493d7d
Jade <jaylan.franklin09@gmail.com>
2023-06-05 17:05:09 -0500

    May be removing event file

--- Aoipy/Classes/Events.py
+++ Aoipy/Classes/Events.py
@@ -4,11 +4,22 @@ from Aoipy.tools import findBracketPairs
 #              EVENTS                 #
 #######################################
 
-
-class AoiEvents:
-    def onReady(self, code):
-        from Aoipy.Classes.AoiPyClient import bots, TotalFuncs
-        # Define on_ready event function
-        @bots.event
-        async def on_ready():
-            await findBracketPairs(code, TotalFuncs, None)
+#
+#class AoiEvents:
+#    def onReady(self, code):
+#        from Aoipy.Classes.AoiPyClient import bots, TotalFuncs
+#        # Define on_ready event function
+#        @bots.event
+#        async def on_ready():
+#            await findBracketPairs(code, TotalFuncs, None)
+#
+#    def onGuildChannelDelete(self):
+#        from Aoipy.Classes.AoiPyClient import bots
+#        from Aoipy.Functions.Events.onChannelDelete import options
+#        @bots.event
+#        async def on_guild_channel_delete(channel):
+#            options["name"] = channel.name
+#            options['id'] = channel.id
+#
+#
+#
