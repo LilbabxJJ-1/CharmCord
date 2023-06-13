53e807ac6b5ef506978d67a0d61ee220a6525468
Jade <jaylan.franklin09@gmail.com>
2023-06-13 01:54:42 -0500

    Add `If`, `ElIf`, and `EndIf`

---
+++ CharmCord/Functions/Python/if.py
@@ -0,0 +1,18 @@
+async def If(args, context):
+    choices = ["==", ">=", "<=", "!=", "<", ">"]
+    for i in choices:
+        if i in args:
+            if i in ["==", "!="]:
+                vals = args.split(i)
+                val1 = vals[0]
+                val2 = vals[1]
+            else:
+                vals = args.split(i)
+                val1 = int(vals[0])
+                val2 = int(vals[1])
+            test = eval(f"val1 {i} val2")
+            if test:
+                return True
+            else:
+                return False
+    return False
