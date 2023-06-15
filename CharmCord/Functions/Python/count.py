7624b956eec3d4bc914a5d04b73959d4d58c1667
Jade <jaylan.franklin09@gmail.com>
2023-06-14 23:20:06 -0500

    Fixed count for strings

--- CharmCord/Functions/Python/count.py
+++ CharmCord/Functions/Python/count.py
@@ -1,5 +1,11 @@
 async def count(code, Context):
-    try:
-        return len(eval(code))
-    except:
-        return len(code)
+    if isinstance(code, list):
+        try:
+            return len(eval(code))
+        except:
+            return len(code)
+    else:
+        try:
+            return len([i for i in code.split(" ") if len(i) > 0])
+        except:
+            return 0
