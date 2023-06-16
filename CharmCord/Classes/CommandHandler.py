d60ce7b8f2903a6d4f5943b859f1c16da4a74d60
Jade <jaylan.franklin09@gmail.com>
2023-06-16 13:23:52 -0500

    able to Find dirs inside dirs

--- CharmCord/Classes/CommandHandler.py
+++ CharmCord/Classes/CommandHandler.py
@@ -4,4 +4,7 @@ import os
 def load_commands(dir):
     for filename in os.listdir(dir):
         if filename.endswith('.py'):
-            exec(f"from {dir}.{filename[:-3]} import *")
+            exec(f"from {dir.replace('/', '.')}.{filename[:-3]} import *")
+        elif os.path.isdir(f"{dir}/{filename}") and not filename.endswith("__"):
+            load_commands(f"{dir}/{filename}")
+
