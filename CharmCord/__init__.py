81a598d5477242d5209b754c00146e0bcc7c9a45
Jade <jaylan.franklin09@gmail.com>
2023-06-21 10:06:56 -0500

    Package Description

--- CharmCord/__init__.py
+++ CharmCord/__init__.py
@@ -1,2 +1,11 @@
+"""
+CharmCord | Discord.py Wrapper
+~~~~~~~~~~~~~~~~~~~
+A basic wrapper of discord.py to make developing discord bots easier
+
+:copyright: (c) 2015-2023 LilbabJJx-1 & (c) 2023-present CharmCord Development
+:license: MIT, see LICENSE for more details.
+"""
+
 from .Classes import *
 from .Functions import *
