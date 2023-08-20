ed6057504bbc354eb8b0a20437434a19c8d84810
Jade <Jaylan.franklin09@gmail.com>
2023-08-20 04:22:03 -0500

    Updated example

--- Examples/Simple Bot.py
+++ Examples/Simple Bot.py
@@ -2,21 +2,27 @@ from CharmCord import CharmClient
 
 bot = CharmClient(prefix="!", case_insensitive=True, intents=("all",))
 
-bot.commands(
-    Name="Send",
-    Code="""
-    $sendMessage[$channelID;Hiya, $username[$authorID] and $args[1]]""",
+bot.command(
+    name="Hey",
+    code="""
+    $argCheck[1;You need at least 1 argument!]
+    $sendMessage[$channelID;Hiya, $username[$authorID] and $args[1]]
+    """,
 )
 
-bot.commands(
-    Name="Toast",
-    Code="""
+bot.slashCommand(
+    name="toast",
+    code="""
     $sendMessage[1112301680839643156; I AM TOAST!!]
-    $pyeval[print('Hey')]""",
+    $console[Hiya]
+    """,
+    description="Toast command"
 )
 
 
-bot.onReady(code="""$console[$botName is online!]""")
+bot.onReady(
+    code="$console[$botName is online!]"
+)
 
 
-bot.run("************************")
+bot.run("TOKEN HERE")
