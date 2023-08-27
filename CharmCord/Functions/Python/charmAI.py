b28d9f5fcf3e066635b70a73b17f34c9c4a47f40
Jade <Jaylan.franklin09@gmail.com>
2023-08-27 00:10:29 -0500

    New Functions

---
+++ CharmCord/Functions/Python/charmAI.py
@@ -0,0 +1,30 @@
+import aiohttp
+
+
+async def charmAI(args, context):
+    api_token = 'gb_api_EFSQ1U9NN1x9Na7HZeQeqdC25C9EzG8UvV1Irr4H'
+
+    # Set up the API base URL
+    base_url = 'https://api.gitbook.com/v1'
+    space_id = "zxDSJg6koIjslgO2E0IN"
+
+    # Define the request body as a dictionary
+    request_body = {
+        "query": args,
+        # Add other search parameters as needed
+    }
+
+    # Use aiohttp to make an asynchronous POST request
+    async with aiohttp.ClientSession() as session:
+        async with session.post(f'{base_url}/spaces/{space_id}/search/ask',
+                                json=request_body,
+                                headers={'Authorization': f'Bearer {api_token}'}) as response:
+            if response.status == 200:
+                books_data = await response.json()
+                try:
+                    answer_text = books_data["answer"]["text"]
+                    return answer_text
+                except KeyError:
+                    return "No Answer.. | Try again"
+            else:
+                return None
---
+++ CharmCord/Functions/Users/hasPerm.py
@@ -0,0 +1,11 @@
+async def hasPerm(args, context):
+    vals = args.split(";")
+    user = vals[0]
+    perm = vals[1].lower()
+    guild = await context.guild.fetch_member(int(user))
+    perms = [i for i in guild.guild_permissions]
+    for i in perms:
+        if perm in i:
+            return True
+    else:
+        return False
