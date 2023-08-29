bd68e4b95e8f076c85cf53fb3ecb44e7a548f0be
Jade <Jaylan.franklin09@gmail.com>
2023-08-29 03:43:50 -0500

    update

--- CharmCord/Functions/Messages/sendEmbed.py
+++ CharmCord/Functions/Messages/sendEmbed.py
@@ -1,11 +1,11 @@
 import discord
-
+from CharmCord.CharmErrorHandling import CharmCordErrors
 from CharmCord.all_functions import newline_char
 
 
 async def sendEmbed(args: str, Context):
     """
-    Ex. $sendEmbed[Channel ID;Title;Message;Color?;Footer?]
+    Ex. $sendEmbed[Channel ID;Title;Message;Color?;image?;Footer?]
     Send an Embed
     """
     from CharmCord.Classes.CharmCord import bots
@@ -17,16 +17,24 @@ async def sendEmbed(args: str, Context):
         title = split[1]
         message = split[2]
         color = split[3]
-        footer = split[4]
+        image = split[4]
+        footer = split[5]
         channel = await bots.fetch_channel(int(channel_id))
         embed = eval(
             f"discord.Embed(title=title, description=message, color=discord.Color.{color.lower()}())"
         )
         embed.set_footer(text=footer)
+        embed.set_image(url=image)
+
         if isinstance(Context, discord.Interaction):
-            await Context.response.send_message(embed=embed)
-            message = await Context.original_response()
-            return message.id
+            try:
+                await Context.response.send_message(embed=embed)
+                message = await Context.original_response()
+                return message.id
+            except:
+                await Context.followup.send(embed=embed)
+                message = await Context.original_response()
+                return message.id
         message = await channel.send(embed=embed)
     except discord.ClientException:
         raise SyntaxError("Can't send empty message!")
@@ -36,13 +44,20 @@ async def sendEmbed(args: str, Context):
         channel_id = split[0]
         title = split[1]
         message = split[2]
-        footer = split[4]
+        image = split[4]
+        footer = split[5]
         channel = await bots.fetch_channel(int(channel_id))
         embed = eval("discord.Embed(title=title, description=message)")
         embed.set_footer(text=footer)
+        embed.set_image(url=image)
         if isinstance(Context, discord.Interaction):
-            await Context.response.send_message(embed=embed)
-            message = await Context.original_response()
-            return message.id
+            try:
+                await Context.response.send_message(embed=embed)
+                message = await Context.original_response()
+                return message.id
+            except:
+                await Context.followup.send(embed=embed)
+                message = await Context.original_response()
+                return message.id
         message = await channel.send(embed=embed)
     return message.id
