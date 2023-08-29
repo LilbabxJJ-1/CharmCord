ec0fc5d8fec5c8d070a2c4fdc7b460a2d62abcd1
Jade <Jaylan.franklin09@gmail.com>
2023-08-29 02:13:20 -0500

    Added `$userID`

---
+++ CharmCord/Functions/Users/userID.py
@@ -0,0 +1,13 @@
+from CharmCord.CharmErrorHandling import CharmCordErrors
+
+
+async def userMention(user: str, context):
+    from CharmCord.Classes.CharmCord import bots
+
+    try:
+        user = int(user.replace("<@", "").replace(">", ""))
+        new_user = await bots.fetch_user(user)
+    except ValueError:
+        CharmCordErrors(f"mention ({user}) in $userID not valid")
+        return
+    return new_user.id
