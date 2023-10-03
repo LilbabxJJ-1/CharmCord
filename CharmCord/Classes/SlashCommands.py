523ce0fe3107b79e65b8cbae3e85c5a241f09667
Jade <Jaylan.franklin09@gmail.com>
2023-10-03 02:28:41 -0500

    Added argument descriptions/types

--- CharmCord/Classes/SlashCommands.py
+++ CharmCord/Classes/SlashCommands.py
@@ -1,20 +1,34 @@
 class SlashCommands:
 
     @staticmethod
-    def slash_command(name, code, args: list, description=None, bot=None):
+    def slash_command(name, code, args: list[dict], description=None, bot=None):
+        types = {1: "str", 2: "int"}
         new_args = []
+        arg_descripts = []
         for i in args:
-            new_args.append(f"{i}: str")
-        needs = {"arguments": args, "codes": code, "bot": bot, "name": name, "description": description}
+            if len(i) != 0:
+                try:
+                    i['description']
+                except KeyError:
+                    i['description'] = "No Description"
+                new_args.append(f"{i['name']}: {types[i['type']]}")
+                arg_descripts.append(f"{i['name']} ({types[i['type']]}): {i['description']}")
+        nl = "\n\t\t\t"
+        needs = {"arguments": args, "codes": code, "bot": bot, "name": name}
         func = f"""
-@bot.tree.command(name=name, description=description)
+@bot.tree.command(name=name)
 async def go(ctx, {', '.join(new_args)}):
+                '''{description}
+                
+                Args:
+                    {nl.join(arg_descripts)}
+                '''
                 from CharmCord.Classes.CharmCord import TotalFuncs
                 from CharmCord.tools import noArguments, slashArgs, findBracketPairs
                 Context = ctx
                 new = []
                 for i in arguments:
-                    new.append(eval(i))
+                    new.append(eval(i['name']))
                 finalCode = await noArguments(codes, TotalFuncs, Context)
                 finalCode = slashArgs(new, finalCode)
                 await findBracketPairs(finalCode, TotalFuncs, Context)
