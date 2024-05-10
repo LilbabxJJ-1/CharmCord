ffa870eeb2293b6d7b779000616556d1146df4e6
Jade <jaylan.franklin09@gmail.com>
2024-05-10 09:22:22 -0500

    Tweaks and error handling

--- CharmCord/CharmErrorHandling.py
+++ CharmCord/CharmErrorHandling.py
@@ -1,8 +1,24 @@
-from colorama import Fore
+from colorama import Fore, Style
 
 
 class CharmErrorHandling:
     pass
+
+
+class CharmCordErrorHandling:
+    def __init__(self, error_msg: str, code_sample: str, command_name: str = None):
+        self.error = error_msg
+        self.command = command_name
+        self.code = code_sample
+
+    def command_error(self):
+        print(Fore.RED + f"[CHARMCORD ERR] Error: {self.error} --> Command '{self.command}' "
+                         f"--> '{self.code}'\n")
+        print(Style.RESET_ALL)
+
+    def internal_error(self):
+        print(Fore.RED + f"[CHARMCORD ERR] Error: {self.error} --> '{self.code}'\n")
+        print(Style.RESET_ALL)
 
 
 def deprecated(reason: str):
--- CharmCord/globeHandler.py
+++ CharmCord/globeHandler.py
@@ -4,18 +4,20 @@ bots = None
 all_vars = None
 lets = {}
 
-
 def update_globals(key: str, value):
     global TotalFuncs
     global bots
     global all_vars
     global lets
+    global activity
     if key.lower().startswith("t"):
         TotalFuncs = value
     elif key.lower().startswith("b"):
         bots = value
     elif key.lower().startswith("l"):
         lets = value
+    elif key.lower().startswith("a"):
+        activity = value
     else:
         all_vars = value
 
